64f3c4f57e323719e08a4e65100c7bd05732d2c3
Jason Ding <760837268@qq.com>
2019-03-18 22:23:17 +0800

    update

--- CAcode.m
+++ CAcode.m
@@ -1,24 +1,25 @@
-%产生C/A码并绘制相关系数
-%
-%mod(a+b,2)代表a和b的模2和
-CODE_LENGTH   = 1023;           %码片数   
-NUM           = 500;            %移位数绝对值
+% *************************************************************************
+% CAcode.m :Generate C/A code and calculate correlation function
+%  
+%           Copyright (C) 2019 by Jason DING , All rights reserved.
+%  
+%    version :$Revision: 1.1 $ $Date: 2019/03/13 $
+%    history : 2019/03/13  1.0  new
+%              2019/03/17  1.1  Fixed G2 generator error 
+% *************************************************************************
 
+% Constants ***************************************************************
+CODE_LENGTH   = 1023;           % Number of chips   
+NUM           = 500;            % Absolute value of shift number
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%转换，{0，1}->{1,-1}
+% Code conversion,{0，1}->{1,-1} ******************************************
 function CA=Trans(CA,CodeLength)
 for i=1:CodeLength
-    if CA(i)==0
-        CA(i)=1;
-    elseif CA(i)==1
-            CA(i)=-1;
-    end
+    CA(i)=(CA(i)==0)*(1)+(CA(i)==1)*(-1);
 end
 end
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%生成相关系数
+% Generate two C/A code correlation functions *****************************
 function Rkl = Relafunc(Rk,Rl,CodeLength,NUM)
 Rkl= zeros(2*NUM+1);
 for k = 1:2*NUM+1
@@ -39,17 +40,16 @@ end
 
 end
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%生成C/A码
+% Generate C/A code *******************************************************
 function C_A = generateC_A(PRN,CodeLength,Delay)
 PRN_SELECTION = [2,6;3,7;4,8;5,9;1,9;2,10;1,8;2,9;3,10;2,3;
                  3,4;5,6;6,7;7,8;8,9;9,10;1,4;2,5;3,6;4,7;
                  5,8;6,9;1,3;4,6;5,7;6,8;7,9;8,10;1,6;2,7;
                  3,8;4,9;5,10;];
-% 这里选择PRN
+% Select PRN
 S1=PRN_SELECTION(PRN,1);
 S2=PRN_SELECTION(PRN,2);
-%G1、G2发生器初始化
+% G1, G2 generator initialization
 G1=ones(1,10);
 G2=ones(1,10);
 C_A=zeros(1,CodeLength);
@@ -59,8 +59,8 @@ for i=1:CodeLength+Delay
     G_1=G1(10);
     G1=[NewBit G1(1:9)];
 
-    NewBit=mod(G2(S1)+G2(S2),2); 
-    G_2i=G2(10);
+    NewBit=mod(G2(10)+G2(9)+G2(8)+G2(6)+G2(3)+G2(2),2); 
+    G_2i=mod(G2(S1)+G2(S2),2);
     G2=[NewBit G2(1:9)];
     
     if i>Delay
